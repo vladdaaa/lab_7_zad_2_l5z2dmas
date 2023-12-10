2f387a056a5ed23254b357ded9b42d016bd38c56
vladdaaa <144156817+vladdaaa@users.noreply.github.com>
2023-12-10 23:36:55 +0700

    Добавьте файлы проекта.

---
+++ lab_7_zad_2_l5z2dmas/lab_7_zad_2_l5z2dmas.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+void vvod_mas(int* a, int n) {//vvodim massive
+
+    for (int i = 0; i < n; i++) {
+        cout << "a[" << i << "]";
+        cin >> a[i];
+    }
+}
+
+int* chetnosti(int* a, int n) {
+    int* b = new int[n];
+    int i, j = 0;
+    for (i = 0; i < n; i += 2, j++) {//chet
+        b[j] = a[i];
+
+    }
+    for (i = 1; i < n; i += 2, j++) {//no-chet
+        b[j] = a[i];
+
+    }
+    return b;
+}
+int main(void)
+{
+    setlocale(LC_ALL, "ru");
+
+    int* a, n;
+    int i, j = 0;
+    cout << "Введите размер массива";
+    cin >> n;
+    a = new int[n];
+    vvod_mas(a, n);
+    int* b = chetnosti(a, n);
+
+    for (i = 0; i < n; i++) {
+        cout << b[i] << endl;
+    }
+    delete[] a;
+    delete[] b;
+}
